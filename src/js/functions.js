1d59ebddecbfef1e5e4c6a642b01364ba3976b91
Wahab Kazmi <wahab.kazmi2000@gmail.com>
2022-09-24 08:36:00 -0700

    graph, cards added

--- src/js/functions.js
+++ src/js/functions.js
@@ -89,3 +89,124 @@ $('[search]').keydown(function() {
     $(`.${dropdownClass}`).css('display', 'none')
   }
 });
+
+
+
+/* Chart start */
+var options = {
+  series: [
+    {
+      data: [10, 20, 15, 30, 35, 30, 45, 59, 30, 35, 25, 29, 15]
+    }
+  ],
+  chart: {
+    type: "area",
+    height: 350,
+    background: "#19191E",
+    dropShadow: {
+      enabled: true,
+      color: "#000"
+    },
+    zoom: {
+      enabled: false
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  markers: {
+    colors: ["#FFFFFF"]
+  },
+  stroke: {
+    curve: "smooth",
+    width: 3,
+    fill: {
+      type: "gradient",
+      gradient: {
+        type: "horizontal",
+        colorStops: [
+          [
+            {
+              offset: 0,
+              color: "#243B55",
+              opacity: 1
+            },
+            {
+              offset: 33,
+              color: "#243B55",
+              opacity: 1
+            },
+            // {
+            //   offset: 80,
+            //   color: "#FFAC2F",
+            //   opacity: 1
+            // },
+            {
+              offset: 99,
+              color: "#243B55",
+              opacity: 1
+            }
+          ]
+        ]
+      }
+    }
+  },
+  fill: {
+    type: "gradient",
+    gradient: {
+      shadeIntensity: 1,
+      type: "vertical",
+      colorStops: [
+        [
+          {
+            offset: 0,
+            color: "#141E30",
+            opacity: 1.0
+          },
+          {
+            offset: 70,
+            color: "#243B55",
+            opacity: 0.2
+          },
+          {
+            offset: 97,
+            color: "#243B55",
+            opacity: 0.0
+          }
+        ]
+      ]
+    }
+  },
+  xaxis: {
+    axisBorder: {
+      show: false
+    },
+    axisTicks: {
+      show: false
+    },
+    labels: {
+      style: {
+        colors: "#aaa"
+      }
+    }
+  },
+  yaxis: {
+    labels: {
+      show: false
+    }
+  },
+  grid: {
+    borderColor: "#212529"
+  },
+  legend: {
+    horizontalAlign: "left"
+  },
+  theme: {
+    mode: "dark"
+  }
+};
+
+var chart = new ApexCharts(document.querySelector("#chart"), options);
+chart.render();
+
+/* Chart end */
